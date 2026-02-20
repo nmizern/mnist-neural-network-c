e71950d879265442c5648f6456279a3168fa319a
Nikita Mizerkin <nmizernmp@gmail.com>
2026-02-20 09:42:11 +0100

    clean up

--- CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/daris/Documents/Saclay/ReseauDeNeurones/nn
-# Build directory: C:/Users/daris/Documents/Saclay/ReseauDeNeurones/nn
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("examples")
-subdirs("tests")
--- docs/RUNNING.md
+++
@@ -1,8 +0,0 @@
-# Build and Run 
-## Build
-
-```powershell
-# Ran from repo root
-cmake -S nn -B nn/build --toolchain C:/vcpkg/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64-windows -DCMAKE_PREFIX_PATH=C:/vcpkg/installed/x64-windows
-cmake --build nn/build --config Debug
-```
